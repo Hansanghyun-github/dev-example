a9b4b8169c83771b90dd107d18778722b892c05d
HSH <han41562@gmail.com>
2024-04-16 17:21:27 +0900

    test: add bad test

--- src/test/java/com/example/devexample/DevExampleApplicationTests.java
+++ src/test/java/com/example/devexample/DevExampleApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.devexample;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class DevExampleApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int badAnswer = 3;
+		assertThat(sum(1, 1)).isEqualTo(badAnswer);
+	}
+
+	private static int sum(int a, int b) {
+		return a + b;
 	}
 
 }
